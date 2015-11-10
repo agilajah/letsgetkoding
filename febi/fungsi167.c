43a19c56371130a7b9afc1449a2376e517038fbd
febiagil <febiagil20@gmail.com>
2015-11-10 19:26:58 +0700

    First Commit

---
+++ febi/fungsi167.c
@@ -0,0 +1,18 @@
+
+tuple ROLL_DICE() {
+/* fungsi ROLL_DICE mengembalikan sebuah tuple dengan dua nilai bertype data integer, nilai pertama adalah hasil acak dari dadu pertama. Nilai kedua adalah hasil acak dari dadu ke dua. Digunakan dua dadu pada permainan ini. */
+
+/* KAMUS */
+
+/* ALGORITMA */
+
+	
+}
+
+void SHOW_OFFERED() {
+/* Procedure menampilkan semua list offered */
+/* KAMUS */
+
+/* ALGORITMA */
+
+}

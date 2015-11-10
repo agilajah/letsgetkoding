e2a807f9cefca6e8db0a65354fefeaf74e448fb4
hasnank <hasnurk@gmail.com>
2015-11-10 17:58:34 +0700

    Create board.c

---
+++ hasna/board.c
@@ -0,0 +1,37 @@
+/*
+ * baru nyoba bikin board 5x5, kalo ukuran kotaknya tetep segini
+ * bisa bikin 7x7. belum dikasih kode buat munculin pemilik dan
+ * letak masing-masing pemain.
+*/
+
+void board(){
+	/* KAMUS */
+	
+	/* ALGORITMA */
+	printf("--------------------------------------------------------\n");
+	printf("|  Start   | Jakarta  |  Bonus   | Surabaya | Deserted |\n");
+	printf("|          |   100k   |          |   100k   |  Island  |\n");
+	printf("|          |          |          |          |          |\n");
+	printf("|          |          |          |          |          |\n");
+	printf("--------------------------------------------------------\n");
+	printf("| Bandung  |                                | Semarang |\n");
+	printf("|   250k   |                                |   150k   |\n");
+	printf("|          |                                |          |\n");
+	printf("|          |                                |          |\n");
+	printf("------------                                ------------\n");
+	printf("|   Tax    |                                |  Chance  |\n");
+	printf("|          |                                |          |\n");
+	printf("|          |                                |          |\n");
+	printf("|          |                                |          |\n");
+	printf("------------                                ------------\n");
+	printf("| Denpasar |                                |Yogyakarta|\n");
+	printf("|   250k   |                                |   150k   |\n");
+	printf("|          |                                |          |\n");
+	printf("|          |                                |          |\n");
+	printf("--------------------------------------------------------\n");
+	printf("|  World   |Balikpapan|  Medan   | Makassar |  World   |\n");
+	printf("|  Travel  |   200k   |   200k   |   200k   |   Cup    |\n");
+	printf("|          |          |          |          |          |\n");
+	printf("|          |          |          |          |          |\n");
+	printf("--------------------------------------------------------\n");
+}
